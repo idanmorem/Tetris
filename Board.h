163974c2e7e4e99f22ff42b92998572954bd30af
idanmorem <37801660+idanmorem@users.noreply.github.com>
2021-04-04 12:50:41 +0300

    Update Board.h

--- Board.h
+++ Board.h
@@ -1,6 +1,5 @@
 #ifndef TETRIS_BOARD_H
 #define TETRIS_BOARD_H
-
 #include "Tetromino.h"
 
 #define COLS 12
@@ -11,15 +10,35 @@
 #define ROTATE_COUNTERCLOCKWISE 3
 #define DROP 4
 #define DOWN 5
+#define PIECE_BLOCKS 5
+#define INITIALX 20
+#define INITIALY 20
+#define XSPACE 20
 
 
 class Board {
-    const int width;
-    const int height;
     char arrowKeys[5];
+    int direction;
+    int board[COLS][ROWS];
 
+    enum {POS_FREE, POS_FILLED};
+    void deleteLine(int tY);
 public:
+    void draw();
+    void start(int tNum);
+    void init();
     void setArrowKeys(const char* keys);
+    int getDirection(char key);
+    void setDirection(int dir) {
+        direction = dir;
+    }
+    bool isGameOver();
+    int getXPos(int pos);
+    int getYPos(int pos);
+    bool isFreeBlock(int tX, int tY);
+    bool isPossible(int tX, int tY, int piece, int rotation);
+    void storePiece(int tX, int tY, int piece, int rotation);
+    void deletePossibleLines();
 };
 
 #endif //TETRIS_BOARD_H
