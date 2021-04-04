cdebd8aa6dcf5950a44e418426d859fe80af8897
idanmorem <37801660+idanmorem@users.noreply.github.com>
2021-04-04 12:51:26 +0300

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -8,3 +8,89 @@ void Board::setArrowKeys(const char *keys)
     arrowKeys[ROTATE_COUNTERCLOCKWISE] = keys[3];
     arrowKeys[DROP] = keys[4];
 }
+
+int Board::getDirection(char key)
+{
+    for(int i = 0; i < 5; i++)
+    {
+        if(key == arrowKeys[i])
+            return i;
+    }
+    return -1;
+}
+
+void Board::init()
+{
+    for(int i = 0; i < COLS; i++)
+        for(int j = 0; j < ROWS; j++)
+            board[i][j] = POS_FREE;
+}
+
+bool Board::isGameOver()
+{
+    for(int i = 0; i < COLS; i++)
+        if(board[i][0] == POS_FILLED)
+            return true;
+
+    return false;
+}
+
+void Board::deleteLine(int tY)
+{
+    for(int j = tY; j > 0 ; j--)
+        for(int i = 0; i < COLS; i++)
+            board[i][j] = board[i][j-1];
+}
+
+//void Board::storePiece(int tX, int tY, int piece, int rotation)
+//{
+//    for(int i1 = tX, i2 = 0; i1 < tX + PIECE_BLOCKS; i1++, i2++)
+//        for(int j1 = tY, j2 = 0; j1 < tY + PIECE_BLOCKS; j1++, j2++)
+//            if(tetrominoes->getKindOfBlock(piece, rotation, j2, i2) != 0)
+//                board[i1][j1] = POS_FILLED;
+//}
+
+void Board::deletePossibleLines()
+{
+    for(int j = 0; j < ROWS; j++)
+    {
+        int i = 0;
+        while (i < COLS)
+        {
+            if(board[i][j] != POS_FILLED)
+                break;
+            i++;
+        }
+        if(i == COLS)
+            deleteLine(j);
+    }
+}
+
+bool Board::isFreeBlock(int tX, int tY)
+{
+    if(board[tX][tY] == POS_FREE)
+        return true;
+    else
+        return false;
+}
+
+void Board::draw()
+{
+    for(int j = 0; j < ROWS + 1; j++) {
+        gotoxy(INITIALX, INITIALY + j); // left board left border
+        std::cout << '|' << std::endl;
+        gotoxy(INITIALX + COLS + 1, INITIALY + j); //left board right border
+        std::cout << '|' << std::endl;
+        gotoxy(INITIALX + XSPACE + COLS + 1,INITIALY + j); //right board left border
+        std::cout << '|' << std::endl;
+        gotoxy(INITIALX + XSPACE + 2 * COLS + 2,INITIALY + j); //right board right border
+        std::cout << '|' << std::endl;
+    }
+    for(int i = 0; i < COLS + 2; i++)
+    {
+        gotoxy(INITIALX + i, INITIALY + ROWS); //left board floor
+        std::cout << '*' << std::endl;
+        gotoxy(INITIALX + i + COLS + XSPACE + 1, INITIALY + ROWS) ;//right board floor
+        std::cout << '*' << std::endl;
+    }
+}
